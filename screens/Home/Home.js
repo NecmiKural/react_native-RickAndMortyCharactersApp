fbc2b37e11aadd07da3c8bc7fc86b063b0e2a4a2
neo <necmibkural@gmail.com>
2024-03-15 22:46:24 +0300

    homepage is scrollable now

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,13 +1,12 @@
-import * as React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Button, View, Text, FlatList} from 'react-native';
 import {ListItem, Avatar} from '@rneui/themed';
-import {useState} from "react";
 
 function Home({navigation}) {
-    const [data, setData] = React.useState(undefined);
-    const [isLoaded, setIsLoaded] = React.useState(false);
-    const [error, setError] = React.useState(null);
-    const [expanded, setExpanded] = React.useState(new Array(0).fill(false));
+    const [data, setData] = useState(undefined);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [error, setError] = useState(null);
+    const [expanded, setExpanded] = useState(new Array(0).fill(false));
 
     const getApiData = async () => {
         try {
@@ -22,9 +21,43 @@ function Home({navigation}) {
         }
     };
 
-    React.useEffect(() => {
+    useEffect(() => {
         getApiData();
-    }, [data])
+    }, [data]);
+
+    const renderItem = ({item, index}) => (
+        <ListItem.Accordion
+            key={index}
+            content={
+                <ListItem.Content>
+                    <ListItem.Title>
+                        Episode Name: {item.name}
+                    </ListItem.Title>
+                    <ListItem.Subtitle>Episode: {item.episode}</ListItem.Subtitle>
+                </ListItem.Content>
+            }
+            isExpanded={expanded[index]}
+            onPress={() => {
+                setExpanded(expanded.map((value, i) => i === index ? !value : value));
+            }}
+        >
+            {
+                item.characters.map((character, i) => (
+                    <ListItem key={i}>
+                        <Avatar
+                            rounded
+                            source={{
+                                uri: character,
+                            }}
+                        />
+                        <ListItem.Content>
+                            <ListItem.Title>John Doe</ListItem.Title>
+                            <ListItem.Subtitle>Principle Engineer</ListItem.Subtitle>
+                        </ListItem.Content>
+                    </ListItem>))
+            }
+        </ListItem.Accordion>
+    );
 
     if (error) {
         return <Text>Error: {error.message}</Text>;
@@ -32,42 +65,12 @@ function Home({navigation}) {
         return <Text>Loading...</Text>;
     } else {
         return (
-            <View>
-                {
-                    data.results.map((item, index) => (
-                        <ListItem.Accordion
-                            key={index}
-                            content={
-                                <ListItem.Content>
-                                    <ListItem.Title>
-                                        Episode Name: {item.name}
-                                    </ListItem.Title>
-                                    <ListItem.Subtitle>Episode: {item.episode}</ListItem.Subtitle>
-                                </ListItem.Content>
-                            }
-                            isExpanded={expanded[index]}
-                            onPress={() => {
-                                setExpanded(expanded.map((value, i) => i === index ? !value : value));
-                            }}
-                        >
-                            {
-                                item.characters.map((character, i) => (
-                                    <ListItem key={i}>
-                                        <Avatar
-                                            rounded
-                                            source={{
-                                                uri: character[i],
-                                            }}
-                                        />
-                                        <ListItem.Content>
-                                            <ListItem.Title>John Doe</ListItem.Title>
-                                            <ListItem.Subtitle>Principle Engineer</ListItem.Subtitle>
-                                        </ListItem.Content>
-                                    </ListItem>))
-                            }
-                        </ListItem.Accordion>
-                    ))
-                }
+            <View style={{flex: 1}}>
+                <FlatList
+                    data={data.results}
+                    renderItem={renderItem}
+                    keyExtractor={(item, index) => index.toString()}
+                />
                 <Button
                     title="Go to Details"
                     onPress={() => navigation.navigate('Episodes')}
