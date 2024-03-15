aa6d80363c05614157cdc4d9071ae72a5062103d
neo <necmibkural@gmail.com>
2024-03-15 21:56:13 +0300

    api connected for test

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,55 +1,88 @@
 import * as React from 'react';
 import {Button, View, Text, FlatList} from 'react-native';
+import {ListItem, Avatar} from '@rneui/themed';
+import {useState} from "react";
 
 function Home({navigation}) {
-
-    // axios ve model kullabılabilir
     const [data, setData] = React.useState(undefined);
+    const [isLoaded, setIsLoaded] = React.useState(false);
+    const [error, setError] = React.useState(null);
+    const [expanded, setExpanded] = React.useState(false);
 
     const getApiData = async () => {
-        let result = await fetch('https://rickandmortyapi.com/api/episode')
+        await fetch('https://rickandmortyapi.com/api/episode')
             .then((response) => response.json())
-            .catch((error) => console.error(error));
-        setData(result);
-        console.log(data.info)
+            .then((result) => {
+                setData(result);
+                setIsLoaded(true);
+            }, (error) => {
+                setError(error);
+                setIsLoaded(true);
+            });
+        console.log(data);
     }
 
     React.useEffect(() => {
-        getApiData()
-    }, [])
+        getApiData();
+    }, [data])
 
-    const renderItem = ({item}) => (
-        <View style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            padding: 10,
-            borderBottomWidth: 1,
-            borderBottomColor: '#ccc'
-        }}>
-            <Text>{item.name}</Text>
-        </View>
-    );
-
-    return (
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>Home Screen</Text>
-            {data ? (
-                <FlatList
-                    data={data.results}
-                    renderItem={renderItem}
-                    keyExtractor={(item) => item.id.toString()}
+    if (error) {
+        return <Text>Error: {error.message}</Text>;
+    } else if (!isLoaded) {
+        return <Text>Loading...</Text>;
+    } else {
+        return (
+            <View>
+                {
+                    data.map(item => (
+                        <ListItem.Accordion
+                            content={
+                                <ListItem.Content>
+                                    <ListItem.Title>
+                                        {item.results.name}
+                                    </ListItem.Title>
+                                    <ListItem.Subtitle>Tap to expand</ListItem.Subtitle>
+                                </ListItem.Content>
+                            }
+                            isExpanded={expanded}
+                            onPress={() => {
+                                setExpanded(!expanded);
+                            }}
+                        >
+                            <ListItem>
+                                <Avatar
+                                    rounded
+                                    source={{
+                                        uri: "https://randomuser.me/api/portraits/men/32.jpg",
+                                    }}
+                                />
+                                <ListItem.Content>
+                                    <ListItem.Title>John Doe</ListItem.Title>
+                                    <ListItem.Subtitle>Principle Engineer</ListItem.Subtitle>
+                                </ListItem.Content>
+                            </ListItem>
+                            <ListItem>
+                                <Avatar
+                                    rounded
+                                    source={{
+                                        uri: "https://randomuser.me/api/portraits/men/36.jpg",
+                                    }}
+                                />
+                                <ListItem.Content>
+                                    <ListItem.Title>Albert</ListItem.Title>
+                                    <ListItem.Subtitle>Staff Engineer</ListItem.Subtitle>
+                                </ListItem.Content>
+                            </ListItem>
+                        </ListItem.Accordion>
+                    ))
+                }
+                <Button
+                    title="Go to Details"
+                    onPress={() => navigation.navigate('Episodes')}
                 />
-            ) : (
-                <Text>Loading...</Text>
-            )}
-            <Button
-                title="Go to Details"
-                onPress={() => navigation.navigate('Episodes')}
-            />
-        </View>
-    );
+            </View>
+        );
+    }
 }
-
 
 export default Home;
