cdb887d8ae05acd8a1cf923f445fbd120f32a4f7
neo <necmibkural@gmail.com>
2024-03-15 20:03:56 +0300

    api connected for test

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import {Button, View, Text, FlatList} from 'react-native';
 
-function Home({ navigation }) {
+function Home({navigation}) {
 
     // axios ve model kullabılabilir
     const [data, setData] = React.useState(undefined);
@@ -9,19 +9,40 @@ function Home({ navigation }) {
     const getApiData = async () => {
         let result = await fetch('https://rickandmortyapi.com/api/episode')
             .then((response) => response.json())
-            .then((json) => console.log(json))
             .catch((error) => console.error(error));
         setData(result);
+        console.log(data.info)
     }
 
     React.useEffect(() => {
         getApiData()
     }, [])
 
+    const renderItem = ({item}) => (
+        <View style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            padding: 10,
+            borderBottomWidth: 1,
+            borderBottomColor: '#ccc'
+        }}>
+            <Text>{item.name}</Text>
+        </View>
+    );
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <Text>Home Screen</Text>
-            <Text>{data}</Text>
+            {data ? (
+                <FlatList
+                    data={data.results}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => item.id.toString()}
+                />
+            ) : (
+                <Text>Loading...</Text>
+            )}
             <Button
                 title="Go to Details"
                 onPress={() => navigation.navigate('Episodes')}
