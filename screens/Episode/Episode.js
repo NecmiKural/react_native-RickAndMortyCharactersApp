c2355d8a30abcc307c000def7ed9bad79f5f4a8d
neo <necmibkural@gmail.com>
2024-03-16 08:38:54 +0300

    episodes page added after clicked a specific episode

--- screens/Episode/Episode.js
+++ screens/Episode/Episode.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, FlatList} from 'react-native';
+import {View, FlatList, StyleSheet} from 'react-native';
+import {Text, Card} from '@rneui/themed';
 
 function Episodes({route}) {
     const {episodeId} = route.params;
@@ -23,14 +24,52 @@ function Episodes({route}) {
         <View>
             <Text>Name: {episode.name}</Text>
             <Text>Episode: {episode.episode}</Text>
-            <Text>Characters:</Text>
+            {/*<Text>Characters:</Text>*/}
             <FlatList
                 data={episode.characters}
-                renderItem={({item}) => <Text>{item}</Text>}
+                renderItem={({item}) =>
+                    <Card>
+                        <Card.Title>Characters</Card.Title>
+                        <Card.Divider/>
+                        {item.map((u, i) => {
+                            return (
+                                <View key={i} style={styles.user}>
+                                    <Image
+                                        style={styles.image}
+                                        resizeMode="cover"
+                                        source={{uri: u.avatar}}
+                                    />
+                                    <Text style={styles.name}>{u.name}</Text>
+                                </View>
+                            );
+                        })}
+                    </Card>
+                }
                 keyExtractor={item => item}
             />
         </View>
     );
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    fonts: {
+        marginBottom: 8,
+    },
+    user: {
+        flexDirection: 'row',
+        marginBottom: 6,
+    },
+    image: {
+        width: 30,
+        height: 30,
+        marginRight: 10,
+    },
+    name: {
+        fontSize: 16,
+        marginTop: 5,
+    },
+});
 export default Episodes;
