2770e8b86631998167d2610623abe1490f4373d4
neo <necmibkural@gmail.com>
2024-03-17 11:01:11 +0300

    unnecessary component removed

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -9,7 +9,6 @@ import {
 import {ListItem} from '@rneui/themed';
 import FavoritesButton from '../../components/FavoritesButton';
 import SearchBar from '../../components/SearchBar';
-import Notification from '../../components/Notification';
 
 function Home({navigation}) {
     const [data, setData] = useState([]);
@@ -96,7 +95,6 @@ function Home({navigation}) {
     } else {
         return (
             <View style={styles.container}>
-                <Notification />
                 <SearchBar holder={"Search Episodes"} term={searchTerm} onTermChange={setSearchTerm}
                            onTermSubmit={() => searchEpisodes(searchTerm)}/>
                 <FlatList
